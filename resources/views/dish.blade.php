f83491aa5816ec4fe853bd269eea83a9b8d33b6f
inertial-brick <karlojura1997vt@gmail.com>
2023-10-23 22:15:01 +0200

    initial commit

---
+++ resources/views/dish.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <title>My Blog</title>
+</head>
+
+
+<body>
+
+
+    <article>
+        <h1><a href="/dish"></a>Moje prvo jelo </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </article>
+
+    <a href="/"> Go Back</a>
+</body>
+
+</html>
---
+++ resources/views/dishesOfTheWorld.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <title>Jela svijeta</title>
+</head>
+
+
+<body>
+    <div class = "mainTitle">
+        <header>
+            <h1>Jela svijeta</h1>
+        </header>
+    </div>
+
+    <article>
+        <h1><a href="/dish"> Moje prvo jelo</a> </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </article>
+</body>
+
+</html>
