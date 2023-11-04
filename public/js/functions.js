c60fd3996bce5e223d7591ddcbae531d822528d1
inertial-brick <karlojura1997vt@gmail.com>
2023-11-04 21:28:41 +0100

    ref: cleanup functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,19 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var addButton = document.getElementById("addIngredient");
-    var ingredientsContainer = document.querySelector(".ingredients");
-    var ingredientCount =
+    const addButton = document.getElementById("addIngredient");
+    const ingredientsContainer = document.querySelector(".ingredients");
+
+    let ingredientCount =
         ingredientsContainer.querySelectorAll(".ingredient").length + 1;
 
     addButton.addEventListener("click", function () {
-        var newIngredient = document.createElement("div");
+        const newIngredient = document.createElement("div");
+
         newIngredient.className = "ingredient";
-        newIngredient.innerHTML =
-            '<input type="text" name="ingredient_' +
-            ingredientCount +
-            '" id="ingredient_' +
-            ingredientCount +
-            '" placeholder="Naziv namirnice">';
+        newIngredient.innerHTML = `
+            <input
+                type="text"
+                name="ingredient_${ingredientCount}"
+                id="ingredient_${ingredientCount}"
+                placeholder="Naziv namirnice"
+            >
+        `;
+
         ingredientsContainer.appendChild(newIngredient);
+
         ingredientCount++;
     });
 });
