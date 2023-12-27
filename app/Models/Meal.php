9e2dc8b1b2109193b0d153a5898be5c2ddf8dada
inertial-brick <karlojura1997vt@gmail.com>
2023-12-27 16:04:49 +0100

    feat: adding meal translatable table

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -4,19 +4,22 @@ namespace App\Models;
 
 use App\Models\Ingredient;
 
-
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Meal extends Model
+class Meal extends Model implements TranslatableContract
 {
 
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Translatable;
 
     public $timestamps = false;
+
+    public $translatedAttributes = ['title', 'description', 'status'];
     protected $fillable = [
         'title',
         'description',
